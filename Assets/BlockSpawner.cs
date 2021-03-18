9270a4668aba814b044814c2a05754f6c11d6278
Tserke <d.Tserkezidis@gmail.com>
2021-03-18 20:39:21 +0200

    CoreGameplay
    Created the core gameplay.

---
+++ Assets/BlockSpawner.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BlockSpawner : MonoBehaviour
+{
+    public Transform[] spawnPoints;
+    public GameObject blockPrefab;
+    public float waveTime = 1f;
+    private float spawnTime = 2f;
+   
+    void Update()
+    {
+        if (Time.time >= spawnTime)
+        { 
+            SpawnBlocks();
+            spawnTime = Time.time + waveTime;
+        }
+    }
+    
+    void SpawnBlocks()
+    {
+        int randomIndex = Random.Range(0, spawnPoints.Length);
+        for (int i = 0; i < spawnPoints.Length; i++)
+        {
+            if (randomIndex != i)
+            {
+                //Quaternion.Identity is used when rotate is not needed
+                Instantiate(blockPrefab, spawnPoints[i].position, Quaternion.identity);
+            }
+        }
+    }
+
+}
--- Assets/Player.cs
+++ Assets/Player.cs
@@ -4,15 +4,26 @@ using UnityEngine;
 
 public class Player : MonoBehaviour
 {
-    // Start is called before the first frame update
+    public float speed = 15f;
+    public float mapWidth = 5f;
+    private Rigidbody2D rb;
+
     void Start()
     {
-        
+        rb = GetComponent<Rigidbody2D>();
     }
 
-    // Update is called once per frame
-    void Update()
+    void FixedUpdate()
     {
-        
+        //player movement
+        float x = Input.GetAxis("Horizontal") * Time.fixedDeltaTime * speed;
+        Vector2 newPosition = rb.position + Vector2.right * x;
+        newPosition.x = Mathf.Clamp(newPosition.x, -mapWidth, mapWidth);
+        rb.MovePosition(newPosition);
+    }
+
+    void OnCollisionEnter2D()
+    {
+        FindObjectOfType<GameManager>().EndGame();
     }
 }
