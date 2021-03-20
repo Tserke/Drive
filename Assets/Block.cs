c16199dd019cef83ed146629dd0b67d84be3a4d5
Tserke <d.Tserkezidis@gmail.com>
2021-03-20 16:09:17 +0200

    End Game Effect, Progressive Difficulty
    Dramatic ending effect and made the game more difficult over time.

--- Assets/Block.cs
+++ Assets/Block.cs
@@ -4,6 +4,11 @@ using UnityEngine;
 
 public class Block : MonoBehaviour
 {
+    public float gravityModifier = 20f;
+     void Start()
+    {
+        GetComponent<Rigidbody2D>().gravityScale += Time.timeSinceLevelLoad / gravityModifier;
+    }
 
     void Update()
     {
--- Assets/GameManager.cs
+++ Assets/GameManager.cs
@@ -5,8 +5,20 @@ using UnityEngine.SceneManagement;
 
 public class GameManager : MonoBehaviour
 {
+    public float slowtime = 10f;
     public void EndGame()
     {
+        StartCoroutine(RestartLevel());
+    }
+
+    IEnumerator RestartLevel()
+    {
+        Time.timeScale = 1f / slowtime;
+        Time.fixedDeltaTime = Time.fixedDeltaTime / slowtime;
+        yield return new WaitForSeconds(1f/slowtime);
+
+        Time.timeScale = 1f;
+        Time.fixedDeltaTime = Time.fixedDeltaTime * slowtime;
         SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex);
     }
 }
